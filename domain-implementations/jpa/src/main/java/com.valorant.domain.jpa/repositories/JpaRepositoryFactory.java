5bbe41ad2ec1bf49101796d09ee1972a84f4a509
Silviu Sebastian Balint <bladesilviu526@gmail.com>
2024-06-19 15:29:21 +0200

    FEAT: Added JpaRepositoryFactory

--- domain-implementations/jpa/src/main/java/com.valorant.domain.jpa/repositories/JpaRepositoryFactory.java
+++ domain-implementations/jpa/src/main/java/com.valorant.domain.jpa/repositories/JpaRepositoryFactory.java
@@ -1,4 +1,44 @@
 package com.valorant.domain.jpa.repositories;
 
+import jakarta.persistence.EntityManager;
+
 public class JpaRepositoryFactory {
+
+    private final EntityManager entityManager;
+
+    public JpaRepositoryFactory(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public JpaAgentRepository createAgentRepository() {
+        return new JpaAgentRepository(entityManager);
+    }
+
+    public JpaMapRepository createMapRepository() {
+        return new JpaMapRepository(entityManager);
+    }
+
+    public JpaMatchRepository createMatchRepository() {
+        return new JpaMatchRepository(entityManager);
+    }
+
+    public JpaPlayerRepository createPlayerRepository() {
+        return new JpaPlayerRepository(entityManager);
+    }
+
+    public JpaWeaponRepository createWeaponRepository() {
+        return new JpaWeaponRepository(entityManager);
+    }
+
+    public JpaMatchPlayerRepository createMatchPlayerRepository() {
+        return new JpaMatchPlayerRepository(entityManager, createMatchRepository());
+    }
+
+    public JpaPlayerAgentRepository createPlayerAgentRepository() {
+        return new JpaPlayerAgentRepository(entityManager, createAgentRepository());
+    }
+
+    public JpaPlayerWeaponRepository createPlayerWeaponRepository() {
+        return new JpaPlayerWeaponRepository(entityManager, createWeaponRepository());
+    }
 }
